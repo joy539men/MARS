123213cfb188fb13a807026070c808a949e219ff
Leo <joy539men@email.com>
2023-10-27 00:50:30 +0800

    add Account. Update Front

--- Account/demo.js
+++
@@ -1,5 +0,0 @@
-$("#tabs").tabs();
-
-$("#btnLogin").on("click",function(){
-    alert("會員您好，您已成功登入")
-})
---
+++ Account_page/demo.js
@@ -0,0 +1,9 @@
+$("#tabs").tabs();
+
+$("#btnLogin1").on("click",function(){
+    alert("歡迎回來，您已成功登入~")
+})
+
+$("#btnLogin2").on("click",function(){
+    alert("您已成功註冊成為會員~")
+})
