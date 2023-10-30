a81702e6e399b0d3a22ead8a3c1b3ddcbf55ad68
Leo <joy539men@email.com>
2023-10-31 00:42:18 +0800

    cartLi

--- Navbar.js
+++ Navbar.js
@@ -1,14 +1,27 @@
-//購物車圖示動態增加、視窗懸浮效果
-$(function(){
+//購物車圖示動態增加
+$(function () {
     var cartItem = 0;
-    $('#addCart').on('click',function(){
+    $('.addCart').on('click', function () {
         cartItem++;
         $('#cartCount').text(cartItem);
     });
-    $('.cartIcon').on('mouseover',function(){
+    $('.cartIcon').on('mouseover', function () {
         $('#cartPopup').show();
     });
-    $('.cartIcon').on('mouseout',function(){
+    $('.cartIcon').on('mouseout', function () {
         $('#cartPopup').hide();
-    })
- });
+    });
+
+    $('.addCart').on('click', function () {
+        var group = $(this).data('group');
+        var selectedQuantity = $('.product-quantity option:selected').val();
+        var item = "商品 " + group + '*' + selectedQuantity + '支'; 
+        addToCart(item);
+    });
+    function addToCart(item) {
+        var cartItemsList = $('#cartItemlist');
+        var listItem = $('<li>').text(item);
+        cartItemsList.append(listItem);
+    }
+});
+
