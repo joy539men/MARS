5581b4b8e9223f24cc79734013cc9ae29a7c5e78
Leo <joy539men@email.com>
2023-10-31 23:27:20 +0800

    GG

--- Navbar.js
+++ Navbar.js
@@ -1,27 +1,48 @@
 //購物車圖示動態增加
-$(function () {
-    var cartItem = 0;
-    $('.addCart').on('click', function () {
-        cartItem++;
-        $('#cartCount').text(cartItem);
-    });
-    $('.cartIcon').on('mouseover', function () {
-        $('#cartPopup').show();
-    });
-    $('.cartIcon').on('mouseout', function () {
-        $('#cartPopup').hide();
-    });
-
-    $('.addCart').on('click', function () {
-        var group = $(this).data('group');
-        var selectedQuantity = $('.product-quantity option:selected').val();
-        var item = "商品 " + group + '*' + selectedQuantity + '支'; 
-        addToCart(item);
-    });
-    function addToCart(item) {
-        var cartItemsList = $('#cartItemlist');
-        var listItem = $('<li>').text(item);
-        cartItemsList.append(listItem);
-    }
+$('.cartIcon').on('click', function () {
+    $('#cartPopup').toggle();
 });
+
+$(document).ready(function () {
+    var cartItems = {}; // 用于存储购物车中的商品及其数量
+    $(".addCart").on("click", function () {
+        var productInfo = $(this).closest(".product"); // 获取最近的包含商品信息的父元素
+
+        var productName = productInfo.find("h3").text(); // 获取商品名称
+        var productImage = productInfo.find(".prdPic").attr("src"); // 获取商品图片链接
+        var productQuantity = parseInt(productInfo.find(".product-quantity").val()); // 获取商品数量
+
+        // 检查购物车中是否已存在相同商品
+        var existingProduct = $("#cartItemlist").find(`li[data-name="${productName}"]`);
+
+        if (existingProduct.length > 0) {
+            var currentQuantity = parseInt(existingProduct.find(".quantity").text());
+            existingProduct.find(".quantity").text(currentQuantity + productQuantity); // 更新现有商品的数量
+        } else {
+            // 创建包含商品信息的 HTML 元素
+            var cartItem = `<li data-name="${productName}">
+                                    <img src="${productImage}" width="50px">
+                                    <span>${productName} </span>&nbsp;&nbsp;&nbsp;數量 : 
+                                    <span class="quantity">${productQuantity}</span>
+                                </li>`;
+
+            // 将商品信息添加到购物车弹出窗口中的购物车列表
+            $("#cartItemlist").append(cartItem);
+        };
+        if (!cartItems[productName]) {
+            cartItems[productName] = 1; // 如果购物车中没有这种商品，则将其数量设为1
+        } else {
+            // 如果购物车中已有这种商品，则不增加数量
+        };
+
+        // 计算购物车中不重复商品的总数
+        var uniqueItemCount = Object.keys(cartItems).length;
+
+        // 在购物车图标上显示不重复商品的总数
+        $('#cartCount').text(uniqueItemCount);
+    });
+});
+
+
+
 
