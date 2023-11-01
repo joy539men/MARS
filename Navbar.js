5780518f81bedb3cc99a62698ba7ed3ea6c1d3ce
Leo <joy539men@email.com>
2023-11-02 00:22:17 +0800

    ...

--- Navbar.js
+++ Navbar.js
@@ -4,12 +4,15 @@ $('.cartIcon').on('click', function () {
 });
 
 $(document).ready(function () {
+
+
+
     var cartItems = {}; // 用于存储购物车中的商品及其数量
     $(".addCart").on("click", function () {
         var productInfo = $(this).closest(".product"); // 获取最近的包含商品信息的父元素
-
         var productName = productInfo.find("h3").text(); // 获取商品名称
         var productImage = productInfo.find(".prdPic").attr("src"); // 获取商品图片链接
+        var productPrice = productInfo.find('p').text();
         var productQuantity = parseInt(productInfo.find(".product-quantity").val()); // 获取商品数量
 
         // 检查购物车中是否已存在相同商品
@@ -20,15 +23,39 @@ $(document).ready(function () {
             existingProduct.find(".quantity").text(currentQuantity + productQuantity); // 更新现有商品的数量
         } else {
             // 创建包含商品信息的 HTML 元素
-            var cartItem = `<li data-name="${productName}">
-                                    <img src="${productImage}" width="50px">
-                                    <span>${productName} </span>&nbsp;&nbsp;&nbsp;數量 : 
-                                    <span class="quantity">${productQuantity}</span>
+            var cartItem = `<li data-name="${productName}" data-price="${productPrice}">
+                                   <img src="${productImage}" width="50px">
+                                   <span>${productName} </span>&nbsp;&nbsp;&nbsp;數量 : 
+                                   <span class="quantity">${productQuantity}</span>&nbsp;&nbsp;&nbsp;
+                                   <span>單價 : ${productPrice}</span>
+                                    <button class="deleteCartItem">删除</button>
                                 </li>`;
-
             // 将商品信息添加到购物车弹出窗口中的购物车列表
             $("#cartItemlist").append(cartItem);
+            updateTotalPrice();
         };
+        function updateTotalPrice() {
+            var totalPrice = 0;
+            $("#cartItemlist li").each(function() {
+                var quantity = $(this).find(".quantityInput").val();
+                var price = $(this).data("price");
+                var itemTotal = quantity * price;
+                totalPrice += itemTotal;
+            });
+    
+            $("#totalPrice").text("Total: $" + totalPrice);
+        }
+    
+        // Delete a cart item and update the total price
+        $("#cartItemlist").on("click", ".deleteCartItem", function() {
+            $(this).parent().remove();
+            updateTotalPrice();
+        });
+    
+        // Update total price when the quantity changes
+        $("#cartItemlist").on("input", ".quantityInput", function() {
+            updateTotalPrice();
+        });
         if (!cartItems[productName]) {
             cartItems[productName] = 1; // 如果购物车中没有这种商品，则将其数量设为1
         } else {
--- Store_page/Store.js
+++ Store_page/Store.js
@@ -4,18 +4,30 @@ $(document).ready(function () {
         {
             imageSrc: '../imges/prd_image_1/突擊 TK-F HS C.jpg',
             title: '突擊 TK-F HS C',
-            price: '售價 NT 7,000',
+            price: '8,787',
         },
         {
             imageSrc: '../imges/prd_image_2/突擊 TK-TTY A.jpg',
             title: '突擊 TK-TTY A',
-            price: '售價 NT 5,656',
-        }
+            price: ' 5,656',
+        },
+        {
+            imageSrc: '../imges/prd_image_3/馭 DriveX 10METALLIC B.jpg',
+            title: '馭 DriveX 10METALLIC B',
+            price: '7,777',
+        },
+        {
+            imageSrc: '../imges/prd_image_4/神速 ARS-90F J.jpg',
+            title: '神速 ARS-90F J',
+            price: ' 6,666',
+        },
     ];
 
     var imageChang = [
         ['../imges/prd_image_1/突擊 TK-F HS C(1).jpg'],
         ['../imges/prd_image_2/突擊 TK-TTY A(1).jpg'],
+        ['../imges/prd_image_3/馭 DriveX 10METALLIC B(1).jpg'],
+        ['../imges/prd_image_4/神速 ARS-90F J(1).jpg'],
     ];
 
     $('.listItem li').each(function (index) {
@@ -26,11 +38,11 @@ $(document).ready(function () {
     });
 
     $('.listItem li .prdPic').each(function (index) {
-        $(this).on('mouseout', function () {
+        $(this).on('mouseleave', function () {
             var originalImage = imageInfo[index].imageSrc;
             $(this).attr('src', originalImage);
         });
-        $(this).on('mouseover', function () {
+        $(this).on('mouseenter', function () {
             var newImagesrc = imageChang[index][0];
             $(this).attr('src', newImagesrc);
         });
