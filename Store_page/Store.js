f8fc16ad70cd868dcf888eee50290919e7be85c6
Leo <joy539men@email.com>
2023-11-08 00:43:55 +0800

    updata store

--- Store_page/Store.js
+++ Store_page/Store.js
@@ -4,30 +4,7 @@ $('.cartIcon').on('click', function () {
 });
 
 $(document).ready(function () {
-    var productInfo = [
-        {
-            id : 1 ,
-            imageSrc: '../imges/prd_image_1/突擊 TK-F HS C.jpg',
-            title: '突擊 TK-F HS C',
-            price: '8,787',
-        },
-        {
-            imageSrc: '../imges/prd_image_2/突擊 TK-TTY A.jpg',
-            title: '突擊 TK-TTY A',
-            price: '5,656',
-        },
-        {
-            imageSrc: '../imges/prd_image_3/馭 DriveX 10METALLIC B.jpg',
-            title: '馭 DriveX 10METALLIC B',
-            price: '7,777',
-        },
-        {
-            imageSrc: '../imges/prd_image_4/神速 ARS-90F J.jpg',
-            title: '神速 ARS-90F J',
-            price: '6,666',
-        },
-    ];
-
+    
     var imageChang = [
         ['../imges/prd_image_1/突擊 TK-F HS C(1).jpg'],
         ['../imges/prd_image_2/突擊 TK-TTY A(1).jpg'],
@@ -35,17 +12,10 @@ $(document).ready(function () {
         ['../imges/prd_image_4/神速 ARS-90F J(1).jpg'],
     ];
 
-    $('.listItem li').each(function (index) {
-        var image = productInfo[index];
-        $(this).find('.prdPic').attr('src', image.imageSrc);
-        $(this).find('h3').text(image.title);
-        $(this).find('p').text(image.price);
-    });
-
-// 更換商品圖片效果
-    $('.listItem li .prdPic').each(function (index) {
+    // 更換商品圖片效果
+    $('.container1 li img').each(function (index) {
         $(this).on('mouseleave', function () {
-            var originalImage = productInfo[index].imageSrc;
+            var originalImage = data[index].imageSrc;
             $(this).attr('src', originalImage);
         });
         $(this).on('mouseenter', function () {
@@ -54,30 +24,47 @@ $(document).ready(function () {
         });
     })
 
-// 商品加入購物車清單內容
-    $('.addCart').click(function(){
+    // 商品加入購物車清單內容
+    $('.addCart').click(function () {
         var pdInfo = $(this).closest(".product");
         var pdSrc = pdInfo.find('.prdPic').attr("src")
         var pdName = pdInfo.find("h3").text();
         var pdPrice = pdInfo.find('p').text();
         var pdQuantity = parseInt(pdInfo.find('.quantity').val());
 
-            $('.cartpop').append(`
-            <tr calss="name">
+        var cartList = `
+            <tr class="${pdName}">
                 <td style="border: 1.3px solid black"><img src="${pdSrc}" style="width:90px">&nbsp;&nbsp;&nbsp;<span>${pdName}</span></td>
                 <td style="border: 1.3px solid black; text-align: center;">NT ${pdPrice} 元</td>
                 <td class="quItem" style="border: 1.3px solid black;  text-align: center;">${pdQuantity}</td>
                 <td style="border: 1.3px solid black;  text-align: center;"><button>刪除</td>
-            </tr>`);
-        
-        
-        
-            
-        
+            </tr>`;
 
+
+
+        $('.cartpop').append(cartList);
 
     });
 });
 
+// 獲取元素 
+var listBox = document.querySelector('.container1 > ul');
+//console.log(listBox)
 
+// 渲染 - 創建節點
+var frg = document.createDocumentFragment();
+
+data.forEach(function (item) {
+    // 創建 li
+    var li = document.createElement('li')
+    li.innerHTML = `
+    <div class="img"><img src="${item.imageSrc}"></div>
+    <p>${item.title}</p>
+    <span>NT ${item.price} 元</span>
+  `
+  // 插入到 frg 裡面
+  frg.appendChild(li)
+})
+// 插入到 ul 裡面
+listBox.appendChild(frg);
 
