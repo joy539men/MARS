a897d313e5b2fcae3310eb697c699195c9b81723
Leo <joy539men@email.com>
2023-11-09 00:47:59 +0800

    stroeJS cartHTML

--- Store_page/data_list.js
+++ Store_page/data_list.js
@@ -1,26 +1,30 @@
 var data = [
     {
-        id : 1 ,
+        prdId : 1 ,
         imageSrc: '../imges/prd_image_1/突擊 TK-F HS C.jpg',
         title: '突擊 TK-F HS C',
         price: '8,787',
+        cartNumber : 0
     },
     {
-        id : 2,
+        prdId : 2,
         imageSrc: '../imges/prd_image_2/突擊 TK-TTY A.jpg',
         title: '突擊 TK-TTY A',
         price: '5,656',
+        cartNumber : 0
     },
     {
-        id : 3 ,
+        prdId : 3 ,
         imageSrc: '../imges/prd_image_3/馭 DriveX 10METALLIC B.jpg',
         title: '馭 DriveX 10METALLIC B',
         price: '7,777',
+        cartNumber : 0
     },
     {
-        id : 4 ,
+        prdId : 4 ,
         imageSrc: '../imges/prd_image_4/神速 ARS-90F J.jpg',
         title: '神速 ARS-90F J',
         price: '6,666',
+        cartNumber : 0
     },
 ]; 
