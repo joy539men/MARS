128ab3d7f2783ac772e111eca48ed972847d48c8
Leo <joy539men@email.com>
2023-11-11 01:53:15 +0800

    cart_update

--- public/Store_page/data_list.js
+++ public/Store_page/data_list.js
@@ -4,70 +4,80 @@ var data = [
         imageSrc: '../imges/prd_image_1/突擊 TK-F HS C.jpg',
         title: '突擊 TK-F HS C',
         price: 8787,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : true,
     },
     {
         prdId : 2,
         imageSrc: '../imges/prd_image_2/突擊 TK-TTY A.jpg',
         title: '突擊 TK-TTY A',
         price: 5656,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 3 ,
         imageSrc: '../imges/prd_image_3/馭 DriveX 10METALLIC B.jpg',
         title: '馭 DriveX 10METALLIC B',
         price: 7777,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 4 ,
         imageSrc: '../imges/prd_image_4/神速 ARS-90F J.jpg',
         title: '神速 ARS-90F J',
         price: 6666,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 5 ,
         imageSrc: '../imges/prd_image_5/JS-T1PRO C.jpg',
         title: 'JS-T1PRO C',
         price: 5555,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 6 ,
         imageSrc: '../imges/prd_image_6/極速 JS-99 O.jpg',
         title: '極速 JS-99 O',
         price: 4646,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 7 ,
         imageSrc: '../imges/prd_image_7/極速 JS-800HT G 穿線拍.jpg',
         title: '極速 JS-800HT G 穿線拍',
         price: 3333 ,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 8 ,
         imageSrc: '../imges/prd_image_8/55週年紀念拍 BRS-12 SE B.jpg',
         title: '55週年紀念拍 BRS-12 SE B',
         price: 2626,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 9 ,
         imageSrc: '../imges/prd_image_9/亮劍 BRS-12N.jpg',
         title: '亮劍 BRS-12N',
         price: 3999,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
     {
         prdId : 10 ,
         imageSrc: '../imges/prd_image_10/突擊TK-66 Q.png',
         title: '突擊TK-66 Q',
         price: 1234,
-        cartNumber : 0
+        cartNumber : 0,
+        isSelect : false,
     },
 
 ]; 
--- public/cart_page/cart.js
+++ public/cart_page/cart.js
@@ -8,58 +8,83 @@ var cartList = document.querySelector('.box > ul');
 if (!cart.length) {
     cartList.innerHTML = '<div class="cartImage"><img src="../imges/shopping-cart.png"><div>您還沒有添加商品，快去選購吧!</div></div>'
 } else {
-    // 創建節點
-    var frg = document.createDocumentFragment();
-    var total = 0;
-    var totalprice = 0;
-    
-    cart.forEach(function (item) {
-        var li = document.createElement('li')
-        var item_price = toThousands(item.price)
-        var subtotal_price = toThousands(item.price * item.cartNumber)
-        li.innerHTML = `
-    <div class="select">
-        <input type="checkbox">
-    </div>
-    <div class="img">
-        <img src="${item.imageSrc}">
-    </div>
-    <div class="desc">${item.title}</div>
-    <div class="price">
-       單價 : $ ${item_price} 元
-    </div>
-    <div class="number">
-        <button ${item.cartNumber == 1 && 'disabled'}>-</button>
-        <input type="text" value="${item.cartNumber}">
-        <button>+</button>
-    </div>
-    <div class="subtotal">
-       小計 $ ${subtotal_price } 元
-    </div>
-    <div class="del">
-        <button>刪除</button>
-    </div>
-     `
-        frg.appendChild(li);
-
-        // 總計、總價
-        total += item.cartNumber
-        totalprice += item.cartNumber * item.price
+    function inti(){
+        var total = 0;
+        var totalprice = 0;
+        var li_innerHTML ='';
+        cart.forEach(function (item,index) {
+            var item_price = toThousands(item.price)
+            var subtotal_price = toThousands(item.price * item.cartNumber)
+            li_innerHTML += `<li>
+            <div class="select">
+                <input type="checkbox" data-id="${index}" class="select-item" ${item.isSelect?"checked":""}/>
+            </div>
+            <div class="img">
+                <img src="${item.imageSrc}">
+            </div>
+            <div class="desc">${item.title}</div>
+            <div class="price">
+                單價 : $ ${item_price} 元
+            </div>
+            <div class="number">
+                <button data-id="${item.id}" class="sub" ${item.cartNumber == 1 && 'disabled'}>-</button>
+                <input type="text" value="${item.cartNumber}">
+                <button>+</button>
+            </div>
+             <div class="subtotal">
+                小計 $ ${subtotal_price } 元
+            </div>
+            <div class="del">
+                <button>刪除</button>
+            </div></li>
+            `
+            // 總計、總價
+            if(item.isSelect === true) {
+            total += item.cartNumber 
+            totalprice += item.cartNumber * item.price
+            }
     });
-    cartList.appendChild(frg);
-
-     var total_price = toThousands(totalprice)
-     var div = document.createElement('div');
+    cartList.innerHTML=li_innerHTML;
+    
+    var total_price = toThousands(totalprice)
+    var div = '';
     div = `
             <p>總價 : ${total_price}</p>
             <button>去結算</button>
             <button>清空購物車</button>
             <button>繼續去購物</button>
             <p>總計 : ${total}</p>
+            <input type="checkbox" id="all"><span>全選</span>
         `
-    $('.bottom').append(div);
+    $('.bottom').html(div);
+    }
+    inti()
 
+    // 判斷時選擇按鈕
+        var box = document.querySelector('.box')
+     box.addEventListener('click',function(e){
+        if (e.target.className === 'select-item'){
+            cart[e.target.dataset.id].isSelect = !cart[e.target.dataset.id].isSelect 
+            inti()
+        }
+    })
+
+    // 判斷是否全選
+    var selectAll = cart.every(function(item){return item.isSelect})
+    if (selectAll){
+        $('#all').prop('checked',true)
+    }
+    // 全選按鈕
+    var all = document.getElementById('all')
+    $('#all').on('click',function(){
+        cart.forEach(function(){
+            $('.select input').prop('checked',all.checked)
+        })
+    })
     
+    
+
+   
     function toThousands(num) {
         var num = (num || 0).toString(), result = '';
         while (num.length > 3) {
@@ -69,10 +94,11 @@ if (!cart.length) {
         if (num) { result = num + result; }
         return result;
     }
-   
-
-
-
-
 }
-//
+// 刪除按鈕 綁定一個 click 事件
+$('.del').on('click',function(){
+    var flag = confirm('確定要刪除商品嗎?')
+    if (flag) {
+        $(this).parent().remove()
+    }
+})
